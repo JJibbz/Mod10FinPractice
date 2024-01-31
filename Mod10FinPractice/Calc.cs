3054ee67c3b682ec6138a8b2c7407256bf190930
JJibbz <vkazancev322@gmail.com>
2024-01-31 17:51:28 +0700

    Добавьте файлы проекта.

---
+++ Mod10FinPractice/Calc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod10FinPractice
+{
+    public class Calc : ICalc
+    {
+        ILogger Logger { get; }
+        public Calc(ILogger logger)
+        {
+            Logger = logger;
+        }
+        public int Sum(int a, int b)
+        {
+            Logger.Event("Складываем...");
+            Thread.Sleep(3000);
+            Logger.Event("Результат сложения:");
+            return a + b;
+            
+
+        }
+    }
+}
---
+++ Mod10FinPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Mod10FinPractice
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Logger = new Logger();
+                int a = 0, b = 0;
+                try
+                {
+                    Console.WriteLine("Введите значение 1 слагаемого");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите значение 2 слагаемого");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Calc calc = new Calc(Logger);
+                    Console.WriteLine(calc.Sum(a, b));
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("Что-то не так...");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+        }
+    }
+}
